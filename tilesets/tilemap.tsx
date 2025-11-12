826f4653f1110d56df6aba29bec63b70f2a8cf4c
longnameislong935 <dhill935@gmail.com>
2025-11-12 18:48:01 +0000

    Test Map for Team

---
+++ tilesets/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilemap" tilewidth="32" tileheight="32" tilecount="266" columns="19">
+ <image source="tilemap.png" width="628" height="475"/>
+</tileset>
