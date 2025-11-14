2fdb98de44a29dbaa17a30da47f03b66a6f9fa1c
longnameislong935 <dhill935@gmail.com>
2025-11-14 10:20:23 +0000

    Change Rooms for teams

---
+++ tilesets/Modern Interiors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Modern Interiors" tilewidth="32" tileheight="32" tilecount="3248" columns="58">
+ <image source="../../../../Downloads/Modern_Interiors_Free_v2.2/Modern tiles_Free/free_overview.png" width="1886" height="1792"/>
+</tileset>
