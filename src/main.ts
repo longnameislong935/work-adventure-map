0c5339572681269247cb686cd2ecc55697b98316
longnameislong935 <dhill935@gmail.com>
2026-01-07 10:57:03 +0000

    Bell

--- src/main.ts
+++ src/main.ts
@@ -9,59 +9,24 @@ let currentPopup: any = undefined;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
 
-    // Existing Clock Logic
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
-        const time = today.getHours() + ":" + (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
-        console.log('Clock area entered at: ' + time);
+        const time = today.getHours() + ":" + today.getMinutes();
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    });
+    })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup);
+    WA.room.area.onLeave('clock').subscribe(closePopup)
 
-    // Bootstrap the Scripting API Extra library
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
 
-        try {
-            // This listens for the 'bell' variable you set in Tiled
-            // @ts-ignore
-            WA.state.onVariableChange('bell').subscribe((value) => {
-                console.log('EVENT: Bell variable changed to:', value);
+}).catch(e => console.error(e));
 
-                if (value === true) {
-                    console.log('Ringing the bell...');
-
-                    // Opens a popup. "bellMessage" refers to the name of the rectangle in Tiled.
-                    currentPopup = WA.ui.openPopup("bellMessage", "Ding Dong! Someone is at the door.", [
-                        {
-                            label: "Close",
-                            className: "success",
-                            callback: (popup: any) => {
-                                popup.close();
-                                currentPopup = undefined;
-                            }
-                        }
-                    ]);
-
-                    // Auto-reset the variable to false after 3 seconds so it can be pressed again
-                    setTimeout(() => {
-                        console.log('Resetting bell variable...');
-                        // @ts-ignore
-                        WA.state.saveVariable('bell', false);
-                    }, 3000);
-                }
-            });
-        } catch (e) {
-            console.error("Variable subscription failed. Check if 'bell' exists in Tiled:", e);
-        }
-
-    }).catch(e => console.error('Error bootstrapping Extra:', e));
-
-}).catch(e => console.error('WA.onInit Error:', e));
-
-function closePopup() {
+function closePopup(){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
